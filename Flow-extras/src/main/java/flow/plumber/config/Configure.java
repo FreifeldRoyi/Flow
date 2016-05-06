dc69a70bf20a26461ad08abbe83ae4835e3cb705
Royi Freifeld <freifeld.royi@gmail.com>
2016-05-06 19:28:18 +0300

    Introduced Configure class & tests

---
+++ Flow-extras/src/main/java/flow/plumber/config/Configure.java
@@ -0,0 +1,56 @@
+package flow.plumber.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author royif
+ * @since 06/05/16
+ */
+public class Configure
+{
+	private Properties properties;
+
+	/**
+	 * Initializes the properties held by this class
+	 *
+	 * @param filename     - non null path to a properties file
+	 * @param defaultsFile - non null path to defaults file
+	 */
+	public Configure(String filename, String defaultsFile)
+	{
+		try (InputStream defaultsStream = getClass().getClassLoader().getResourceAsStream(defaultsFile);
+				InputStream propertiesStream = getClass().getClassLoader().getResourceAsStream(filename))
+		{
+			Properties defaults = new Properties();
+			defaults.load(defaultsStream);
+			this.properties = new Properties(defaults);
+			if (propertiesStream != null)
+			{
+				this.properties.load(propertiesStream);
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			//TODO log
+			throw new RuntimeException(e);
+		}
+		catch (IOException e)
+		{
+			//TODO log
+			throw new RuntimeException(e);
+		}
+	}
+
+	public Configure(String defaultsFile)
+	{
+		this("", defaultsFile);
+	}
+
+	public String getProperty(String propertyName)
+	{
+		return this.properties.getProperty(propertyName);
+	}
+}
